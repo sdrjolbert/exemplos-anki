64e55434f1494e25c700bca7e4284d3b6d18bae6
Jolbert Sodré <jolbert.sodre15@gmail.com>
2024-06-30 00:05:05 -0300

    Finalizar correção dos scripts de teste

--- src/converterApkgIntoFolder.js
+++ src/converterApkgIntoFolder.js
@@ -1,19 +1,21 @@
-const JSZip = require('jszip');
-const fs = require('fs-extra');
-const path = require('path');
+const JSZip = require("jszip");
+const fs = require("fs-extra");
+const path = require("path");
 
 async function extractAPKG(filePath, outputDir) {
   try {
     const buffer = await fs.readFile(filePath);
     const zip = await JSZip.loadAsync(buffer);
 
-    await Promise.all(Object.keys(zip.files).map(async (filename) => {
-      const fileData = await zip.files[filename].async("nodebuffer");
-      const outputFile = path.join(outputDir, filename);
+    await Promise.all(
+      Object.keys(zip.files).map(async (filename) => {
+        const fileData = await zip.files[filename].async("nodebuffer");
+        const outputFile = path.join(outputDir, filename);
 
-      await fs.ensureDir(path.dirname(outputFile));
-      await fs.writeFile(outputFile, fileData);
-    }));
+        await fs.ensureDir(path.dirname(outputFile));
+        await fs.writeFile(outputFile, fileData);
+      })
+    );
 
     console.log("Extração concluída com sucesso!");
   } catch (e) {
@@ -24,13 +26,18 @@ async function extractAPKG(filePath, outputDir) {
 const args = process.argv.slice(2);
 
 if (args.length < 1) {
-  console.error("Uso: node converterApkgIntoFolder.js '<caminho_do_arquivo.apkg>'");
+  console.error(
+    "Uso: node converterApkgIntoFolder.js '<caminho_do_arquivo.apkg>'"
+  );
   process.exit(1);
 }
 
 const filePath = args[0];
 
 const fileNameWithoutExtension = path.parse(filePath).name;
-const outputDir = fileNameWithoutExtension;
+const outputDir = path.resolve(
+  __dirname,
+  `../decks/${fileNameWithoutExtension}`
+);
 
-extractAPKG(filePath, `../decks/${outputDir}`);
+extractAPKG(filePath, outputDir);
